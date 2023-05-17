d713bc5910ef90dc207400281ec05140e7e2a960
Daniel Carvalho <daniel.devop@gmail.com>
2023-05-17 19:48:55 -0300

    criando um servidor http que atende na porta 3333

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,2 +1,9 @@
-const msg: string = 'Olá'
-console.log(msg)
+import fastify from 'fastify'
+
+const app = fastify()
+
+app.listen({
+  port: 3333
+}).then(()=>{
+  console.log('HTTP server running on http://localhost:3333')
+})
