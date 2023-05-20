834c42fb67113af457d35f902cfe29e4caad845c
Daniel Carvalho <daniel.devop@gmail.com>
2023-05-20 09:07:55 -0300

    teste do tailwind com o prettier

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,3 +1,7 @@
 export default function Home() {
-  return <h1>Olá mundo</h1>
+  return (
+    <div className="m-10 h-20 w-20 rounded bg-red-500 text-2xl font-bold uppercase">
+      Oi
+    </div>
+  )
 }
