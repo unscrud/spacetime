f2ccb0215231754b86b8ca3f91ae684f0e2f7101
Daniel Carvalho <daniel.devop@gmail.com>
2023-05-20 12:05:17 -0300

    instalação e configuração do tail e do nativewind

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,20 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View className='bg-gray-950 flex-1 items-center justify-center'>
+      <Text className='text-zinc-50 font-bold text-4xl'>Olá mundo!</Text>
+      <StatusBar style="light" translucent />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
