dd0796ddc9b09ece26601a6906a6e605eec8b424
Daniel Carvalho <daniel.devop@gmail.com>
2023-05-21 10:03:35 -0300

    testando fontes configuradas no tema do tailwind

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,7 +1,8 @@
 export default function Home() {
   return (
     <div className="m-10 h-20 w-20 rounded bg-red-500 text-2xl font-bold uppercase">
-      Oi
+      <h1 className="font-sans">Olá</h1>
+      <h1 className="font-alt">Mundo!</h1>
     </div>
   )
 }
