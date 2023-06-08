6964cd143af338ae41f384832deeb6678a39b6ee
Daniel Carvalho <daniel.devop@gmail.com>
2023-06-08 15:55:39 -0300

    Testando uma propriedade

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -5,7 +5,9 @@ export default function Home() {
     <div className="m-10 h-20 w-20 rounded bg-red-500 text-2xl font-bold uppercase">
       <h1 className="font-sans">Olá</h1>
       <h1 className="font-alt">Mundo!</h1>
-      <Button />
+      <Button title="btn A" />
+      <Button title="btn B" />
+      <Button title="btn C" />
     </div>
   )
 }
