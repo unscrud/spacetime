00b5751e20fb4084fa3dc5cb669ae746345ae2ce
Daniel Carvalho <daniel.devop@gmail.com>
2023-06-09 08:34:10 -0300

    Borda no meio

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <main className="grid min-h-screen grid-cols-2">
       {/* Left */}
-      <div className="relative flex flex-col items-start justify-between overflow-hidden px-28 py-16">
+      <div className="relative flex flex-col items-start justify-between overflow-hidden border-r border-white/10 px-28 py-16">
         {/* Blur */}
         <div className="absolute right-0 top-1/2 h-[288px] w-[526px] -translate-y-1/2 translate-x-1/2 rounded-full  bg-purple-700 opacity-50 blur-full" />
 
