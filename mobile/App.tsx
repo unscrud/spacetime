e71c43c69c96f66a4c6eb1ac4c08ca279e3d543a
Daniel Carvalho <daniel.devop@gmail.com>
2023-06-24 19:45:21 -0300

    retorno nulo se fontes não carregadas para não exibir nada na tela

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,7 +1,23 @@
+import { BaiJamjuree_700Bold } from '@expo-google-fonts/bai-jamjuree'
+import {
+  Roboto_400Regular,
+  Roboto_700Bold,
+  useFonts,
+} from '@expo-google-fonts/roboto'
 import { StatusBar } from 'expo-status-bar'
 import { Text, View } from 'react-native'
 
 export default function App() {
+  const [hasLoadedFonts] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold,
+    BaiJamjuree_700Bold,
+  })
+
+  if (!hasLoadedFonts) {
+    return null
+  }
+
   return (
     <View className="flex-1 items-center justify-center bg-gray-950">
       <Text className="text-4xl font-bold text-zinc-50">Olá mundo!</Text>
