f596014766552724ee1d06584137473ecfb2d57f
Daniel Carvalho <daniel.devop@gmail.com>
2023-06-24 13:43:20 -0300

    sessão hero

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,5 +1,6 @@
 import { User } from 'lucide-react'
-
+import Image from 'next/image'
+import logo from '../assets/logo.svg'
 export default function Home() {
   return (
     <main className="grid min-h-screen grid-cols-2">
@@ -24,6 +25,27 @@ export default function Home() {
             memórias
           </p>
         </a>
+
+        {/* Hero */}
+        <div className="space-y-5">
+          <Image src={logo} alt="Logo da SpaceTime" />
+          <div className="max-w-[420px] space-y-1">
+            <h1 className=" text-5xl font-bold leading-tight text-gray-50">
+              Sua cápsula do tempo
+            </h1>
+            <p className="text-lg leading-relaxed">
+              Colecione momentos marcantes a sua jornada e compartilhe (se
+              quiser) com o mundo!
+            </p>
+
+            <a
+              className="inline-block rounded-full bg-green-500 px-5 py-3 font-alt text-sm font-semibold uppercase leading-none text-black hover:bg-green-600"
+              href=""
+            >
+              CADASTRAR LEMBRANÇA
+            </a>
+          </div>
+        </div>
       </div>
 
       {/* Right */}
