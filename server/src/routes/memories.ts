485e4486872f15f99c2d1f4a24252ac8b9473535
Daniel Carvalho <daniel.devop@gmail.com>
2023-07-03 08:34:17 -0300

    busca de uma única memória

--- server/src/routes/memories.ts
+++ server/src/routes/memories.ts
@@ -1,4 +1,5 @@
 import { FastifyInstance } from 'fastify'
+import { z } from 'zod'
 import { prisma } from '../lib/prisma'
 export async function memoriesRoutes(app: FastifyInstance) {
   app.get('/memories', async () => {
@@ -17,8 +18,21 @@ export async function memoriesRoutes(app: FastifyInstance) {
     })
   })
 
-  // app.get('/memories/:id', async () => {
-  // })
+  app.get('/memories/:id', async (request) => {
+    const paramsSchema = z.object({
+      id: z.string().uuid(),
+    })
+
+    const { id } = paramsSchema.parse(request.params)
+
+    const memory = await prisma.memory.findUniqueOrThrow({
+      where: {
+        id,
+      },
+    })
+
+    return memory
+  })
 
   // app.post('/memories/:id', async () => {
   // })
