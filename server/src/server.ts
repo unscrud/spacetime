a60c1556bcd59f507568bd2418134eeef935f683
Daniel Carvalho <daniel.devop@gmail.com>
2023-07-03 11:49:41 -0300

    registro da rota de usuários

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,11 @@
 import fastify from 'fastify'
 import { memoriesRoutes } from './routes/memories'
+import { usersRoutes } from './routes/users'
 
 const app = fastify()
 
 app.register(memoriesRoutes)
+app.register(usersRoutes)
 
 app.listen({ port: 3333 }).then(() => {
   console.log('🚀 HTTP server running on http://localhost:3333')
