1a626c1ba3d894024066cd05b03b552d4e2f83ac
Daniel Carvalho <daniel.devop@gmail.com>
2023-07-05 07:53:03 -0300

    obtendo o token  usando o id e a secret do client

--- server/src/routes/auth.ts
+++ server/src/routes/auth.ts
@@ -1,3 +1,4 @@
+import axios from 'axios'
 import { FastifyInstance } from 'fastify'
 import z from 'zod'
 
@@ -7,5 +8,19 @@ export async function authRoutes(app: FastifyInstance) {
       code: z.string(),
     })
     const { code } = bodySchema.parse(request.body)
+    const accessTokenResponse = await axios.post(
+      'https://github.com/login/oauth/access_token',
+      null,
+      {
+        params: {
+          client_id: process.env.GITHUB_CLIENT_ID,
+          client_secret: process.env.GITHUB_CLIENT_SECRET,
+          code,
+        },
+        headers: {
+          Accept: 'application/json',
+        },
+      },
+    )
   })
 }
