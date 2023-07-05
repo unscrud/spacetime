27c353b69c450b71cf18d26bd991f46908e80159
Daniel Carvalho <daniel.devop@gmail.com>
2023-07-05 10:21:47 -0300

    registro e definição da secret (temporária) do jwt

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,6 +1,7 @@
 import 'dotenv/config'
 
 import cors from '@fastify/cors'
+import jwt from '@fastify/jwt'
 import fastify from 'fastify'
 import { authRoutes } from './routes/auth'
 import { memoriesRoutes } from './routes/memories'
@@ -11,6 +12,11 @@ const app = fastify()
 app.register(cors, {
   origin: true,
 })
+
+app.register(jwt, {
+  secret: 'spacetime',
+})
+
 app.register(authRoutes)
 app.register(memoriesRoutes)
 app.register(usersRoutes)
