e3395659a9d621055b76a4d501935e1ea3352d17
Daniel Carvalho <daniel.devop@gmail.com>
2023-07-05 07:39:13 -0300

    validação dos dados do request.body com zod

--- server/src/routes/auth.ts
+++ server/src/routes/auth.ts
@@ -1,5 +1,11 @@
 import { FastifyInstance } from 'fastify'
+import z from 'zod'
 
 export async function authRoutes(app: FastifyInstance) {
-  app.post('/register', async (request) => { })
+  app.post('/register', async (request) => {
+    const bodySchema = z.object({
+      code: z.string(),
+    })
+    const { code } = bodySchema.parse(request.body)
+  })
 }
