66a1fde608172ab7864e6bb626457a03f22909fb
Daniel Carvalho <daniel.devop@gmail.com>
2023-07-06 10:35:29 -0300

    obtem os parâmetros devolvidos na url de callback

--- web/src/app/api/auth/callback/route.ts
+++ web/src/app/api/auth/callback/route.ts
@@ -1,3 +1,5 @@
-import { NextRequest } from 'next/server';
+import { NextRequest } from 'next/server'
 
-export async function GET(request: NextRequest) { }
+export async function GET(request: NextRequest) {
+  const { searchParams } = new URL(request.url)
+}
