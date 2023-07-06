888e1d8e001d647c5d403d692f219ad7de93e97a
Daniel Carvalho <daniel.devop@gmail.com>
2023-07-06 10:36:20 -0300

    pega o código dos parâmetros

--- web/src/app/api/auth/callback/route.ts
+++ web/src/app/api/auth/callback/route.ts
@@ -2,4 +2,5 @@ import { NextRequest } from 'next/server'
 
 export async function GET(request: NextRequest) {
   const { searchParams } = new URL(request.url)
+  const code = searchParams.get('code')
 }
