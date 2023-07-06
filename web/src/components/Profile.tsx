2168e8e586c6a40e82fa47aa9b1646827085177f
Daniel Carvalho <daniel.devop@gmail.com>
2023-07-06 15:20:15 -0300

    bloco do avatar do usuário logado

--- web/src/components/Profile.tsx
+++ web/src/components/Profile.tsx
@@ -1,9 +1,26 @@
 import { getUser } from '@/lib/auth'
+import Image from 'next/image'
 
 export function Profile() {
   const { name, avatarUrl } = getUser()
 
   console.log(name, avatarUrl)
 
-  return <p> oi </p>
+  return (
+    <div className="flex items-center gap-3 text-left">
+      <Image
+        src={avatarUrl}
+        width={40}
+        height={40}
+        alt=""
+        className="h-10 w-10 rounded-full"
+      />
+      <p className="max-w-[140px] text-sm leading-snug">
+        Bem vindo <span className="underline">{name}</span>
+        <a href="" className="block text-red-400 hover:text-red-300">
+          Quero sair
+        </a>
+      </p>
+    </div>
+  )
 }
