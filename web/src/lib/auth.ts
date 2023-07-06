f91cace44c4f798bd099b706ee94d79b160b7965
Daniel Carvalho <daniel.devop@gmail.com>
2023-07-06 11:28:35 -0300

    pegando o valor do token

--- web/src/lib/auth.ts
+++ web/src/lib/auth.ts
@@ -1,5 +1,11 @@
+import { cookies } from 'next/headers'
+
 interface User {
   sub: string
   name: string
   avatarUrl: string
+}
+
+export function getUser(): User {
+  const token = cookies().get('token')?.value
 }
