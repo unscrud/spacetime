911542b712a58bd50ea1d0cec81d222291f8e955
Daniel Carvalho <daniel.devop@gmail.com>
2023-07-09 13:53:06 -0300

    código comum a todas as páginas

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -1,7 +1,14 @@
+import { EmptyMemories } from '@/components/EmptyMemories'
+import { Hero } from '@/components/Hero'
+import { Profile } from '@/components/Profile'
+import { Signin } from '@/components/Signin'
+
+import { Copyright } from '@/components/Copyright'
 import {
   Bai_Jamjuree as BaiJamjuree,
   Roboto_Flex as Roboto,
 } from 'next/font/google'
+import { cookies } from 'next/headers'
 import { ReactNode } from 'react'
 import './globals.css'
 
@@ -18,12 +25,35 @@ export const metadata = {
 }
 
 export default function RootLayout({ children }: { children: ReactNode }) {
+  const isAutenticated = cookies().has('token')
+
   return (
     <html lang="en">
       <body
         className={`${roboto.variable} ${baiJamjuree.variable} bg-gray-900 font-sans text-gray-100`}
       >
         {children}
+        <main className="grid min-h-screen grid-cols-2">
+          {/* Left */}
+          <div className="relative flex flex-col items-start justify-between overflow-hidden border-r border-white/10 bg-[url(../assets/bg-stars.svg)] bg-cover px-28 py-16">
+            {/* Blur */}
+            <div className="absolute right-0 top-1/2 h-[288px] w-[526px] -translate-y-1/2 translate-x-1/2 rounded-full  bg-purple-700 opacity-50 blur-full" />
+
+            {/* Stripes */}
+            <div className="absolute bottom-0 right-2 top-0 w-2 bg-stripes" />
+
+            {isAutenticated ? <Profile /> : <Signin />}
+
+            <Hero />
+
+            <Copyright />
+          </div>
+
+          {/* Right */}
+          <div className="flex flex-col bg-[url(../assets/bg-stars.svg)] bg-cover p-16">
+            <EmptyMemories />
+          </div>
+        </main>
       </body>
     </html>
   )
