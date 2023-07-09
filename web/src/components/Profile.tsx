dbea5d7812a3880dd0ac54cd1780b8febb1ef0a6
Daniel Carvalho <daniel.devop@gmail.com>
2023-07-09 18:00:07 -0300

    link para a rota de logout

--- web/src/components/Profile.tsx
+++ web/src/components/Profile.tsx
@@ -15,7 +15,10 @@ export function Profile() {
       />
       <p className="max-w-[140px] text-sm leading-snug">
         Bem vindo <span className="underline">{name}</span>
-        <a href="" className="block text-red-400 hover:text-red-300">
+        <a
+          href="/api/auth/logout"
+          className="block text-red-400 hover:text-red-300"
+        >
           Quero sair
         </a>
       </p>
