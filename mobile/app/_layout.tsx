05ebe29e2d265d10eb2617b5c5bb25c2eccdd104
Daniel Carvalho <daniel.devop@gmail.com>
2023-07-10 16:50:40 -0300

    arquivo com elementos comuns a todas as telas

---
+++ mobile/app/_layout.tsx
@@ -0,0 +1,41 @@
+import { BaiJamjuree_700Bold } from '@expo-google-fonts/bai-jamjuree'
+import {
+  Roboto_400Regular,
+  Roboto_700Bold,
+  useFonts,
+} from '@expo-google-fonts/roboto'
+import { SplashScreen } from 'expo-router'
+import { StatusBar } from 'expo-status-bar'
+import { styled } from 'nativewind'
+import { ImageBackground } from 'react-native'
+import blurBg from '../src/assets/luz.png'
+import Stripes from '../src/assets/stripes.svg'
+
+const StiledStripes = styled(Stripes)
+
+export default function Layout() {
+  const [hasLoadedFonts] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold,
+    BaiJamjuree_700Bold,
+  })
+
+  if (!hasLoadedFonts) {
+    return <SplashScreen />
+  }
+
+  return (
+    <ImageBackground
+      source={blurBg}
+      imageStyle={{
+        position: 'absolute',
+        left: '-100%',
+      }}
+      className="relative flex-1 items-center bg-gray-900 px-8 py-10"
+    >
+      <StiledStripes className="absolute left-2" />
+
+      <StatusBar style="light" translucent />
+    </ImageBackground>
+  )
+}
