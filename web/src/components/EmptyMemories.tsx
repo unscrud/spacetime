962c3e3584671f495072b45c0f217e83be85bf3f
Daniel Carvalho <daniel.devop@gmail.com>
2023-07-10 16:06:47 -0300

    Link para  nova memória

--- web/src/components/EmptyMemories.tsx
+++ web/src/components/EmptyMemories.tsx
@@ -1,11 +1,13 @@
+import Link from 'next/link'
+
 export function EmptyMemories() {
   return (
     <div className="flex flex-1 items-center justify-center">
       <p className="w-[360px] text-center leading-relaxed">
         Você ainda não registrou nenhuma lembrança, começe a{' '}
-        <a className="underline hover:text-gray-50" href="">
+        <Link className="underline hover:text-gray-50" href="/memories/new">
           criar agora
-        </a>
+        </Link>
       </p>
     </div>
   )
