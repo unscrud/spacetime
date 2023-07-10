fa46939457958e662bc812d03cf7976e5cc0b7d0
Daniel Carvalho <daniel.devop@gmail.com>
2023-07-10 08:24:26 -0300

    Link de retorno à home

--- web/src/app/memories/new/page.tsx
+++ web/src/app/memories/new/page.tsx
@@ -1,3 +1,16 @@
+import { ChevronLeft } from 'lucide-react'
+import Link from 'next/link'
+
 export default function NewMemory() {
-  return <h1>New</h1>
+  return (
+    <div className="flex flex-1 flex-col gap-4">
+      <Link
+        href="/"
+        className="flex items-center gap-1 text-sm text-gray-200 hover:text-gray-100"
+      >
+        <ChevronLeft className="h-4 w-4" />
+        Voltar à timeline
+      </Link>
+    </div>
+  )
 }
