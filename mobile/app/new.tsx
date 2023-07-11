a77b6ba36b15802cc847fdc43a6f5bd97ed7ed16
Daniel Carvalho <daniel.devop@gmail.com>
2023-07-11 08:01:50 -0300

    link de retorno para memories

--- mobile/app/new.tsx
+++ mobile/app/new.tsx
@@ -1,4 +1,7 @@
+import Icon from '@expo/vector-icons/Feather'
+import { Link } from 'expo-router'
 import { View } from 'react-native'
+import { TouchableOpacity } from 'react-native-gesture-handler'
 import { useSafeAreaInsets } from 'react-native-safe-area-context'
 import Logo from '../src/assets/logo.svg'
 
@@ -12,6 +15,12 @@ export default function NewMemory() {
     >
       <View className=" mt-4 flex-row items-center justify-between">
         <Logo />
+
+        <Link href="/memories" asChild>
+          <TouchableOpacity className="h-10 w-10 items-center justify-center rounded-full bg-purple-500">
+            <Icon name="arrow-left" size={16} color="#FFF" />
+          </TouchableOpacity>
+        </Link>
       </View>
     </View>
   )
