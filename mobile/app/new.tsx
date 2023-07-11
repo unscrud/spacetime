c7552d8c15074f393ad4d5d59e489b99f2370d58
Daniel Carvalho <daniel.devop@gmail.com>
2023-07-11 08:01:00 -0300

    espaçamento top e bottom dentro da área útil

--- mobile/app/new.tsx
+++ mobile/app/new.tsx
@@ -1,10 +1,16 @@
 import { View } from 'react-native'
+import { useSafeAreaInsets } from 'react-native-safe-area-context'
 import Logo from '../src/assets/logo.svg'
 
 export default function NewMemory() {
+  const { bottom, top } = useSafeAreaInsets()
+
   return (
-    <View className="flex-1 px-8">
-      <View>
+    <View
+      className="flex-1 px-8"
+      style={{ paddingBottom: bottom, paddingTop: top }}
+    >
+      <View className=" mt-4 flex-row items-center justify-between">
         <Logo />
       </View>
     </View>
