119ff7bb1f4784d9bc180856e7d0b7e3df73688b
Daniel Carvalho <daniel.devop@gmail.com>
2023-07-11 08:03:10 -0300

    toggle button para tornar memória pública

--- mobile/app/new.tsx
+++ mobile/app/new.tsx
@@ -1,6 +1,7 @@
 import Icon from '@expo/vector-icons/Feather'
 import { Link } from 'expo-router'
-import { View } from 'react-native'
+import { useState } from 'react'
+import { Switch, Text, View } from 'react-native'
 import { TouchableOpacity } from 'react-native-gesture-handler'
 import { useSafeAreaInsets } from 'react-native-safe-area-context'
 import Logo from '../src/assets/logo.svg'
@@ -8,6 +9,7 @@ import Logo from '../src/assets/logo.svg'
 export default function NewMemory() {
   const { bottom, top } = useSafeAreaInsets()
 
+  const [isPublic, setIsPublic] = useState(false)
   return (
     <View
       className="flex-1 px-8"
@@ -21,6 +23,20 @@ export default function NewMemory() {
             <Icon name="arrow-left" size={16} color="#FFF" />
           </TouchableOpacity>
         </Link>
+      </View>
+
+      <View className="mt-6 space-y-6">
+        <View className="flex-row items-center gap-2">
+          <Switch
+            value={isPublic}
+            onValueChange={setIsPublic}
+            thumbColor={isPublic ? '#9b79ea' : '#9e9ea0'}
+            trackColor={{ false: '#767577', true: '#372560' }}
+          />
+          <Text className="font-body text-base text-gray-200">
+            Tornar memória pública
+          </Text>
+        </View>
       </View>
     </View>
   )
