93ae9e6b450c37aca91b39d9126231ad6c672aff
Daniel Carvalho <daniel.devop@gmail.com>
2023-07-11 17:10:12 -0300

    transformando view em scrollview para rolar a tela

--- mobile/app/new.tsx
+++ mobile/app/new.tsx
@@ -1,7 +1,7 @@
 import Icon from '@expo/vector-icons/Feather'
 import { Link } from 'expo-router'
 import { useState } from 'react'
-import { Switch, Text, TextInput, View } from 'react-native'
+import { ScrollView, Switch, Text, TextInput, View } from 'react-native'
 import { TouchableOpacity } from 'react-native-gesture-handler'
 import { useSafeAreaInsets } from 'react-native-safe-area-context'
 import Logo from '../src/assets/logo.svg'
@@ -11,9 +11,9 @@ export default function NewMemory() {
 
   const [isPublic, setIsPublic] = useState(false)
   return (
-    <View
+    <ScrollView
       className="flex-1 px-8"
-      style={{ paddingBottom: bottom, paddingTop: top }}
+      contentContainerStyle={{ paddingBottom: bottom, paddingTop: top }}
     >
       <View className=" mt-4 flex-row items-center justify-between">
         <Logo />
@@ -65,6 +65,6 @@ export default function NewMemory() {
           <Text className="font-alt text-sm uppercase text-black">Salvar</Text>
         </TouchableOpacity>
       </View>
-    </View>
+    </ScrollView>
   )
 }
