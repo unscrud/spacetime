bdba663ec064340db99b792ff25f2b719290d1b6
Daniel Carvalho <daniel.devop@gmail.com>
2023-07-12 19:28:03 -0300

    recebe um arquivo de no máximo 5mb

--- server/src/routes/upload.ts
+++ server/src/routes/upload.ts
@@ -3,5 +3,11 @@ import { FastifyInstance } from 'fastify'
 export async function uploadRoutes(app: FastifyInstance) {
   // para testar...
   // http POST localhost:3333/upload file@/path/to/file.txt -f
-  app.post('/upload', async (request, reply) => { })
+  app.post('/upload', async (request, reply) => {
+    const upload = await request.file({
+      limits: {
+        fileSize: 5_242_880, // 5MB
+      },
+    })
+  })
 }
