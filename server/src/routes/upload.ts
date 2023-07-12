030b866e9eb2793647195643d0428f7294498c5c
Daniel Carvalho <daniel.devop@gmail.com>
2023-07-12 19:27:09 -0300

    rota de upload via post

--- server/src/routes/upload.ts
+++ server/src/routes/upload.ts
@@ -1,6 +1,7 @@
-import { FastifyInstance } from 'fastify';
+import { FastifyInstance } from 'fastify'
 
 export async function uploadRoutes(app: FastifyInstance) {
   // para testar...
   // http POST localhost:3333/upload file@/path/to/file.txt -f
+  app.post('/upload', async (request, reply) => { })
 }
