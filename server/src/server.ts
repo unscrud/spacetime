5f775252479aec8ccd0712adb22767579304c081
Daniel Carvalho <daniel.devop@gmail.com>
2023-07-12 19:12:16 -0300

    registrando o multipart e rota de upload no server

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,12 +2,16 @@ import 'dotenv/config'
 
 import cors from '@fastify/cors'
 import jwt from '@fastify/jwt'
+import multipart from '@fastify/multipart'
 import fastify from 'fastify'
 import { authRoutes } from './routes/auth'
 import { memoriesRoutes } from './routes/memories'
+import { uploadRoutes } from './routes/upload'
 import { usersRoutes } from './routes/users'
 
 const app = fastify()
+
+app.register(multipart)
 
 app.register(cors, {
   origin: true,
@@ -19,6 +23,7 @@ app.register(jwt, {
 
 app.register(authRoutes)
 app.register(memoriesRoutes)
+app.register(uploadRoutes)
 app.register(usersRoutes)
 
 app.listen({ port: 3333, host: '0.0.0.0' }).then(() => {
