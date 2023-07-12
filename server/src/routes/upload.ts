1ade7096bcda55dae1d5015c57532631000e14ba
Daniel Carvalho <daniel.devop@gmail.com>
2023-07-12 19:50:44 -0300

    passando o arquivo para a stream

--- server/src/routes/upload.ts
+++ server/src/routes/upload.ts
@@ -2,6 +2,10 @@ import { FastifyInstance } from 'fastify'
 import { randomUUID } from 'node:crypto'
 import { createWriteStream } from 'node:fs'
 import { extname, resolve } from 'node:path'
+import { pipeline } from 'node:stream'
+import { promisify } from 'node:util'
+
+const pump = promisify(pipeline)
 
 export async function uploadRoutes(app: FastifyInstance) {
   // para testar...
@@ -32,5 +36,9 @@ export async function uploadRoutes(app: FastifyInstance) {
     const writeStream = createWriteStream(
       resolve(__dirname, '../../uploads/', fileName),
     )
+
+    await pump(upload.file, writeStream)
+
+    return { ok: true }
   })
 }
