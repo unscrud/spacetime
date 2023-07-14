99daf1d4ceed410caecebc85fc152144b6ebb9ca
Daniel Carvalho <daniel.devop@gmail.com>
2023-07-14 19:42:04 -0300

    inclusão do preview de vídeo

--- web/src/components/MediaPicker.tsx
+++ web/src/components/MediaPicker.tsx
@@ -5,6 +5,8 @@ import { ChangeEvent, useState } from 'react'
 export function MediaPicker() {
   const [preview, setPreview] = useState<string | null>(null)
 
+  const [fileType, setFileType] = useState<string | null>(null)
+
   function onFileSelected(event: ChangeEvent<HTMLInputElement>) {
     const { files } = event.target
 
@@ -13,8 +15,10 @@ export function MediaPicker() {
     }
 
     const previewURL = URL.createObjectURL(files[0])
-
     setPreview(previewURL)
+
+    const mimeTypeOfFile = files[0].type
+    setFileType(mimeTypeOfFile)
   }
 
   return (
@@ -23,15 +27,23 @@ export function MediaPicker() {
         onChange={onFileSelected}
         type="file"
         id="media"
-        accept="image/*"
+        accept="image/*,video/*"
         className="invisible h-0 w-0"
       />
 
-      {preview && (
+      {preview && fileType && fileType.includes('image') && (
         // eslint-disable-next-line
         <img
           src={preview}
           alt=""
+          className="aspect-video h-[280px] w-full rounded-lg object-cover"
+        />
+      )}
+
+      {preview && fileType && fileType.includes('video') && (
+        <video
+          src={preview}
+          controls={false}
           className="aspect-video h-[280px] w-full rounded-lg object-cover"
         />
       )}
