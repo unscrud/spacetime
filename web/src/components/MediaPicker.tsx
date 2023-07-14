3845b8a5ead4ccb1091df9d52f9f1b23203ee26a
Daniel Carvalho <daniel.devop@gmail.com>
2023-07-14 09:18:59 -0300

    imprime lista de arquivos ao selecionar

--- web/src/components/MediaPicker.tsx
+++ web/src/components/MediaPicker.tsx
@@ -1,9 +1,15 @@
 'use client'
 
+import { ChangeEvent } from 'react'
+
 export function MediaPicker() {
+  function onFileSelected(event: ChangeEvent<HTMLInputElement>) {
+    console.log(event.target.files)
+  }
+
   return (
     <input
-      onChange={(value) => console.log(value)}
+      onChange={onFileSelected}
       type="file"
       id="media"
       className="invisible h-0 w-0"
