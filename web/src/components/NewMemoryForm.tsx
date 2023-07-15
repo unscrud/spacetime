04ab9da3273b89dc600f7756212beaf1dae6df93
Daniel Carvalho <daniel.devop@gmail.com>
2023-07-15 08:06:19 -0300

    botão salvar e impressão dos dados do formulário

--- web/src/components/NewMemoryForm.tsx
+++ web/src/components/NewMemoryForm.tsx
@@ -1,10 +1,18 @@
 'use client'
 import { MediaPicker } from '@/components/MediaPicker'
 import { Camera } from 'lucide-react'
+import { FormEvent } from 'react'
 
 export function NewMemoryForm() {
+  function handleCreateMemory(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault()
+
+    const formData = new FormData(event.currentTarget)
+    console.log(Array.from(formData.entries()))
+  }
+
   return (
-    <form action="" className="flex flex-1 flex-col gap-2">
+    <form onSubmit={handleCreateMemory} className="flex flex-1 flex-col gap-2">
       <div className="flex items-center gap-4">
         <label
           htmlFor="media"
@@ -37,6 +45,13 @@ export function NewMemoryForm() {
         className=" w-full flex-1 resize-none rounded border-0 bg-transparent p-0 text-lg leading-relaxed text-gray-100 placeholder:text-gray-400 focus:ring-0"
         placeholder="Fique livre para adicionar fotos, vídeos e relatos sobre essa experiência que você quer lembrar para sempre."
       />
+
+      <button
+        className="inline-block self-end rounded-full bg-green-500 px-5 py-3 font-alt text-sm font-semibold uppercase leading-none text-black hover:bg-green-600"
+        type="submit"
+      >
+        SALVAR
+      </button>
     </form>
   )
 }
