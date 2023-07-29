5a88c5b66b8d320e86bff0b8508c94ba4c13c1f5
Daniel Carvalho <daniel.devop@gmail.com>
2023-07-29 12:48:25 -0300

    estrutura básica + botão de nova memória

--- mobile/app/memories.tsx
+++ mobile/app/memories.tsx
@@ -1,9 +1,27 @@
-import { Text, View } from 'react-native'
+import Icon from '@expo/vector-icons/Feather'
+import { Link } from 'expo-router'
+import { ScrollView, View } from 'react-native'
+import { TouchableOpacity } from 'react-native-gesture-handler'
+import { useSafeAreaInsets } from 'react-native-safe-area-context'
+import Logo from '../src/assets/logo.svg'
 
-export default function Memories() {
+export default function NewMemory() {
+  const { bottom, top } = useSafeAreaInsets()
+
   return (
-    <View className="flex-1 items-center justify-center">
-      <Text className="text-white">Memories</Text>
-    </View>
+    <ScrollView
+      className="flex-1 px-8"
+      contentContainerStyle={{ paddingBottom: bottom, paddingTop: top }}
+    >
+      <View className=" mt-4 flex-row items-center justify-between">
+        <Logo />
+
+        <Link href="/new" asChild>
+          <TouchableOpacity className="h-10 w-10 items-center justify-center rounded-full bg-green-500">
+            <Icon name="plus" size={16} color="#000" />
+          </TouchableOpacity>
+        </Link>
+      </View>
+    </ScrollView>
   )
 }
