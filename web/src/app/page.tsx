5cd134f78f010f6de41a54a48cedfbcc085782ec
Daniel Carvalho <daniel.devop@gmail.com>
2023-07-30 17:46:20 -0300

    se não autenticado, exibe lista vazia

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,5 +1,10 @@
 import { EmptyMemories } from '@/components/EmptyMemories'
+import { cookies } from 'next/headers'
 
 export default function Home() {
-  return <EmptyMemories />
+  const isAutenticated = cookies().has('token')
+
+  if (!isAutenticated) {
+    return <EmptyMemories />
+  }
 }
