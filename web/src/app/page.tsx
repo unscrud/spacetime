b90f4835d0a11e143f82ab05bad26f7cdbfe9436
Daniel Carvalho <daniel.devop@gmail.com>
2023-08-04 18:04:53 -0300

    link para a memória

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -2,8 +2,10 @@ import { EmptyMemories } from '@/components/EmptyMemories'
 import { api } from '@/lib/api'
 import dayjs from 'dayjs'
 import ptBr from 'dayjs/locale/pt-br'
+import { ArrowRight } from 'lucide-react'
 import { cookies } from 'next/headers'
 import Image from 'next/image'
+import Link from 'next/link'
 
 dayjs.locale(ptBr)
 
@@ -53,6 +55,13 @@ export default async function Home() {
             <p className="text-lg leading-relaxed text-gray-100">
               {memory.excerpt}
             </p>
+            <Link
+              href={`/memories/${memory.id}`}
+              className="flex items-center gap-2 text-sm text-gray-200 hover:text-gray-100"
+            >
+              Ler mais
+              <ArrowRight className="h-4 w-4" />
+            </Link>
           </div>
         )
       })}
